e932cfa90715c5ae34e543f025e3a9b9296894c1
matthew <wb2847@163.com>
2017-06-06 22:37:44 +0800

    1

---
+++ 图/Graph_array/Graph_array/main.c
@@ -0,0 +1,22 @@
+//
+//  main.c
+//  Graph_array
+//
+//  Created by matthew on 2017/6/4.
+//  Copyright © 2017年 matthew. All rights reserved.
+//
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#define INFINTY  65535
+#define MAX_VERTEX_NUM  20
+typedef enum {DG,DN,UDG,UDN} GraphKind;
+typedef struct ArcCell{
+    
+};
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
